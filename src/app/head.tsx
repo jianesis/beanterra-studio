86c99945148134d65fd976b57cf843a305e95e52
davidjian0905 <davidjian0905@gmail.com>
2024-08-16 22:23:31 +0800

    last update

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,6 +4,11 @@ export default function Head() {
       <title>Beanterra Studio</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="The ultimate tech team" />
+      <script
+        src="https://www.google.com/recaptcha/api.js"
+        async
+        defer
+      ></script>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
--- src/components/sections/Banner.tsx
+++ src/components/sections/Banner.tsx
@@ -2,10 +2,12 @@ import styles from "./banner.module.css";
 
 export default function Banner() {
   return (
-    <section id="banner" className={styles.bannerContainer}>
-      <h1 className={styles.mainText + " mb-3"}>Beanterra Studio</h1>
-      <p className={styles.paragraphText}>The Web5 Metaverse Studio</p>
-      <p className={styles.paragraphText}>Bridging the technology gap</p>
-    </section>
+    <div className={styles.bannerContainer}>
+      <section id="banner" style={{ height: "100%" }}>
+        <h1 className={styles.mainText + " mb-3"}>Beanterra Studio</h1>
+        <p className={styles.paragraphText}>The Web5 Metaverse Studio</p>
+        <p className={styles.paragraphText}>Bridging the technology gap</p>
+      </section>
+    </div>
   );
 }
